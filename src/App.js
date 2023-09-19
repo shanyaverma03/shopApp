90206b5141f1e7bef7a4ade56291b92062ca7ec1
Shanya <shanyaverma03@gmail.com>
2023-09-20 00:34:45 +0530

    set up routing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom"; //helps us to define the routes that we want to support in this app
+
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter([{ path: "/", element: <HomePage /> }]);
+
 function App() {
-  return (
-    <div>
-      <header></header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
+
+//call this function and pass the array of route definition objects
+//path- define the path for which this route will be active
+//the value passed to the router prop should be the router created by using the above function
---
+++ src/Components/Navigation.js
@@ -0,0 +1,14 @@
+const Navigation = () => {
+  return (
+    <ul>
+      <li>Shop</li>
+      <li>Products</li>
+      <li>Cart</li>
+      <li>Orders</li>
+      <li>Add Product</li>
+      <li>Add Product</li>
+    </ul>
+  );
+};
+
+export default Navigation;
