537dac71465b539c37d64070cf2d0d1ed97e704b
Shanya <shanyaverma03@gmail.com>
2023-09-20 15:45:27 +0530

    Remove unnecessary links from nav and add basic css

--- client/src/Components/Navigation.js
+++ client/src/Components/Navigation.js
@@ -1,20 +1,12 @@
 import { Link } from "react-router-dom";
 
+import classes from "./Navigation.module.css";
+
 const Navigation = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/shop">Shop</Link>
-            <Link to="/products">Products</Link>
-            <Link to="/cart">Cart</Link>
-            <Link to="/orders">Orders</Link>
-            <Link to="/admin/add-product">Add Product</Link>
-            <Link to="/admin-products">Admin Products</Link>
-          </li>
-        </ul>
-      </nav>
+    <header className={classes.navigation}>
+      <Link to="/products">Products</Link>
+      <Link to="/admin/add-product">Add Product</Link>
     </header>
   );
 };
