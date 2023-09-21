d5c6192832872c0ad150be0976188724b111cf94
Shanya <shanyaverma03@gmail.com>
2023-09-22 03:26:14 +0530

    Fetch cart from backend in client

--- client/src/Components/shop/Cart.js
+++ client/src/Components/shop/Cart.js
@@ -1,4 +1,20 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 const Cart = () => {
+  useEffect(() => {
+    const getCart = async () => {
+      try {
+        const response = await axios.get("/cart");
+        console.log(response.data.items);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    getCart();
+  }, []);
+
   return <h1>Cart</h1>;
 };
 
