a2c6a2f138bbe506204435e8d0a2ddb20ccf0e97
Shanya <shanyaverma03@gmail.com>
2023-09-22 01:20:41 +0530

    Add model for users

---
+++ models/user.js
@@ -0,0 +1,39 @@
+const getDb = require("../util/database");
+const mongodb = require("mongodb");
+
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+  }
+
+  save() {
+    const db = getDb();
+    return db
+      .collection("users")
+      .insertOne(this)
+      .then((user) => {
+        console.log(user);
+        return user;
+      })
+      .catch((err) => {
+        return err;
+      });
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db
+      .collection("users")
+      .find({ _id: new mongodb.ObjectId(userId) })
+      .next()
+      .then((user) => {
+        return user;
+      })
+      .catch((err) => {
+        return err;
+      });
+  }
+}
+
+module.exports = User;
