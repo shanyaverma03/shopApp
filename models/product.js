3c8ad0b3665b2936943d892b5244da94d5cbf032
Shanya <shanyaverma03@gmail.com>
2023-09-22 01:04:22 +0530

    Fix add product functionality in product model

--- models/product.js
+++ models/product.js
@@ -8,7 +8,7 @@ class Product {
     this.description = description;
     this.image = image;
     this.price = price;
-    this._id = new mongodb.ObjectId(_id);
+    this._id = _id ? new mongodb.ObjectId(_id) : null;
   }
 
   save() {
