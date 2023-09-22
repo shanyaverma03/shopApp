ad2f0c16230c13b5dbdb38b7a39f04ab5cd91338
Shanya <shanyaverma03@gmail.com>
2023-09-22 05:50:15 +0530

    Display all orders in client

---
+++ client/src/Components/shop/OrderItem.js
@@ -0,0 +1,18 @@
+import classes from "./OrderItem.module.css";
+
+const OrderItem = (props) => {
+  return (
+    <div className={classes.wrapper}>
+      <div className={classes.card}>
+        <div>Order= # {props.id}</div>
+        {props.items.map((product) => (
+          <div key={product.id}>
+            {product.title} ({product.quantity})
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default OrderItem;
--- client/src/Components/shop/Orders.js
+++ client/src/Components/shop/Orders.js
@@ -1,12 +1,16 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import OrderItem from "./OrderItem";
 
 const Orders = () => {
+  const [orders, setOrders] = useState([]);
+
   useEffect(() => {
     const getOrders = async () => {
       try {
         const response = await axios.get("/orders");
-        console.log(response);
+        console.log(response.data);
+        setOrders(response.data);
       } catch (err) {
         console.log(err);
       }
@@ -17,6 +21,9 @@ const Orders = () => {
   return (
     <div>
       <h1>Orders</h1>
+      {orders.map((order) => (
+        <OrderItem key={order._id} id={order._id} items={order.items} />
+      ))}
     </div>
   );
 };
