580790e21d76aa9a66373bc3a9c68582c26d00e2
Shanya <shanyaverma03@gmail.com>
2023-09-24 20:00:04 +0530

    Add the user middleware again to store a mongoose user model in req.user

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,7 +9,7 @@ exports.addProduct = (req, res, next) => {
     description,
     image,
     price,
-    userId: req.session.user,
+    userId: req.user,
   });
 
   product
--- server.js
+++ server.js
@@ -35,6 +35,20 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.use(productRoutes);
 app.use(cartRoutes);
 app.use(orderRoutes);
