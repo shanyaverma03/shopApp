930e875f0fab7c0cfae0b40495a90481f392eddb
Shanya <shanyaverma03@gmail.com>
2023-10-08 14:11:16 +0530

    Add states in the signup form for email, pw and confirm pw values

--- client/src/Components/auth/Signup.js
+++ client/src/Components/auth/Signup.js
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
@@ -8,6 +8,22 @@ import classes from "./Auth.module.css";
 const Signup = () => {
   const { setIsLoggedIn } = useContext(AuthContext);
   const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const onEmailChangeHandler = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const onPasswordChangeHandler = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const onConfirmPasswordChangeHandler = (event) => {
+    setConfirmPassword(event.target.value);
+  };
 
   const onSubmitHandler = async (event) => {
     event.preventDefault();
@@ -30,6 +46,8 @@ const Signup = () => {
           placeholder="Enter Email"
           name="email"
           required
+          onChange={onEmailChangeHandler}
+          value={email}
         ></input>
 
         <label>
@@ -40,6 +58,8 @@ const Signup = () => {
           placeholder="Enter Password"
           name="password"
           required
+          onChange={onPasswordChangeHandler}
+          value={password}
         ></input>
         <label>
           <b>Confirm Password</b>
@@ -49,6 +69,8 @@ const Signup = () => {
           placeholder="Enter Password"
           name="password"
           required
+          onChange={onConfirmPasswordChangeHandler}
+          value={confirmPassword}
         ></input>
 
         <button>Signup</button>
