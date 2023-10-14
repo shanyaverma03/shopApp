7d40b966b33d358c130ad6ae24635ce06548787f
Shanya <shanyaverma03@gmail.com>
2023-10-14 15:10:48 +0530

    Add validation for email

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 const bcrypt = require("bcrypt");
+const check = require("express-validator");
 
 const User = require("../models/user");
+const { validationResult } = check;
 
 exports.login = async (req, res, next) => {
   const { email, enteredPassword } = req.body;
@@ -36,6 +38,13 @@ exports.logout = (req, res, next) => {
 
 exports.signup = async (req, res, next) => {
   const { email, password, confirmPassword } = req.body;
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log("errors is not empty");
+    console.log(errors.array());
+    //return res.status(422).send(errors.array()[0].msg);
+    return res.send(errors.array()[0].msg);
+  }
   try {
     const findUser = await User.findOne({ email });
     if (findUser) {
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,34 @@
 const express = require("express");
+const check = require("express-validator");
 
 const authController = require("../controllers/authController");
 
 const router = express.Router();
+const checkFunction = check.check;
+const body = check.body;
 
 router.post("/login", authController.login);
 router.post("/logout", authController.logout);
-router.post("/signup", authController.signup);
+router.post(
+  "/signup",
+  [
+    checkFunction("email")
+      .isEmail()
+      .withMessage("Please enter a valid email")
+      .custom((value, { req }) => {
+        if (value === "admin@admin.com") {
+          throw new Error("Can't enter this email");
+        }
+        return true;
+      }),
+    body(
+      "password",
+      "Please enter a password with only numbers and text and at least 6 characters"
+    )
+      .isLength({ min: 6 })
+      .isAlphanumeric(),
+  ],
+  authController.signup
+);
 
 module.exports = router;
