83bda0d6644be76a3616fe7b77d1fc62482b90a6
Shanya <shanyaverma03@gmail.com>
2023-10-15 13:22:21 +0530

    Fix error for destination

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ store.on("error", function (error) {
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, images);
+    cb(null, "images");
   },
   filename: (req, file, cb) => {
     cb(null, new Date().toISOString() + "-" + file.originalname);
