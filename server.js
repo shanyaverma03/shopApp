20e1cdc557f1ad7e9f79f877743ddfd3f088f79e
Shanya <shanyaverma03@gmail.com>
2023-10-23 23:57:51 +0530

    Add error handling middleware

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
 const multer = require("multer");
 
 const productRoutes = require("./routes/productRoutes");
@@ -16,14 +14,6 @@ dotenv.config();
 const User = require("./models/user");
 
 const app = express();
-const store = new MongoDBStore({
-  uri: "mongodb://localhost:27017/Shop" || process.env.MONGO_CONNECT_SESSION,
-  collection: "sessions",
-});
-
-store.on("error", function (error) {
-  console.log(error);
-});
 
 const fileStorage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -51,33 +41,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
   multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
 );
-app.use(
-  session({
-    secret: process.env.MY_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    store: store,
-  })
-);
-
-app.use((req, res, next) => {
-  if (!req.session.user) {
-    return next();
-  }
-  User.findById(req.session.user._id)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
 
 app.use(productRoutes);
 app.use(cartRoutes);
 app.use(orderRoutes);
 app.use(authRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message });
+});
 
 mongoose
   .connect("mongodb://localhost:27017/Shop" || process.env.MONGO_CONNECT)
